3176dbbf03c960282496f4513a508599c01bc9b4
lekaf <tony.seperoumal@gmail.com>
2015-08-19 15:37:51 +0200

    mot du jours

--- app/Manager/TermManager.php
+++ app/Manager/TermManager.php
@@ -4,5 +4,15 @@
 
 	class TermManager extends \W\Manager\Manager
 	{
+		public function getCurrentWordOfTheDay()
+		{
+			$dbh = $this->dbh;
+			$table = $this->table;
+			
+			$sql ="SELECT * FROM terms WHERE is_wotd = 1";
+			$sth = $this->dbh->prepare($sql);
+			$sth->execute();
 
+			return $sth->fetch();
+		}
 	}
